1f039a16ffc06091a9a4c34c10477d55d66619ea
Javi <jmarin0802@gmail.com>
2020-04-18 13:55:09 -0400

    visit agregado y modificacion en pet

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,28 +2,33 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet extends BaseEntity{
-private PetType petType;
-private Owner owner;
-private LocalDate birtDate;
-public PetType getPetType() {
-	return petType;
-}
-public void setPetType(PetType petType) {
-	this.petType = petType;
-}
-public Owner getOwner() {
-	return owner;
-}
-public void setOwner(Owner owner) {
-	this.owner = owner;
-}
-public LocalDate getBirtDate() {
-	return birtDate;
-}
-public void setBirtDate(LocalDate birtDate) {
-	this.birtDate = birtDate;
-}
+public class Pet extends BaseEntity {
+	private PetType petType;
+	private Owner owner;
+	private LocalDate birtDate;
 
+	public PetType getPetType() {
+		return petType;
+	}
+
+	public void setPetType(PetType petType) {
+		this.petType = petType;
+	}
+
+	public Owner getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Owner owner) {
+		this.owner = owner;
+	}
+
+	public LocalDate getBirtDate() {
+		return birtDate;
+	}
+
+	public void setBirtDate(LocalDate birtDate) {
+		this.birtDate = birtDate;
+	}
 
 }
