4d01d4bff5830bb82748dc8d12c4c4d8a8c35fe2
Javi <jmarin0802@gmail.com>
2020-05-01 21:51:07 -0400

    establecer las tablas para el jpa

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,34 +2,58 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
-	
+
+	@Column(name = "name")
 	private String name;
+
+	@ManyToOne
+	@JoinColumn(name = "type_id")
 	private PetType petType;
+
+	@ManyToOne
+	@JoinColumn(name = "owner_id")
 	private Owner owner;
+
+	@Column(name = "birth_date")
 	private LocalDate birtDate;
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public PetType getPetType() {
 		return petType;
 	}
+
 	public void setPetType(PetType petType) {
 		this.petType = petType;
 	}
+
 	public Owner getOwner() {
 		return owner;
 	}
+
 	public void setOwner(Owner owner) {
 		this.owner = owner;
 	}
+
 	public LocalDate getBirtDate() {
 		return birtDate;
 	}
+
 	public void setBirtDate(LocalDate birtDate) {
 		this.birtDate = birtDate;
 	}
