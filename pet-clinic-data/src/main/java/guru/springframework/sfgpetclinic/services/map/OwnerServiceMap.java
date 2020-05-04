cc2f7bf33189d92e483c9f9c1495e1e3214462b5
Javi <jmarin0802@gmail.com>
2020-05-04 13:19:06 -0400

    cargado el vetsdjpa service

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -4,6 +4,7 @@ import java.util.Set;
 
 import javax.management.RuntimeErrorException;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import guru.springframework.sfgpetclinic.model.Owner;
